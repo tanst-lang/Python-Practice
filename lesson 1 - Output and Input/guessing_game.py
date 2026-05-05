e2cf1695924c22ebcdd0d9f7a3efa342e3d55aa1
tanst-lang <tanst@wsc.school.nz>
2026-05-05 14:57:02 +1200

    lesson 1 (slide 43 done)

---
+++ lesson 1 - Output and Input/guessing_game.py
@@ -0,0 +1,34 @@
+### Create a 3-part guessing game ###
+
+# Give your user the first hint and wait for input
+
+# Give your user the second hint and wait for input
+
+# Give your user the final hint and wait for input
+
+# Tell your user the answer
+
+
+# ------------------------------
+
+# EXTENSION
+# Create another guessing game
+
+
+# ------------------------------
+
+# EXPERT (for those who already know some Python)
+# Your 3-part guessing game should have:
+# + An introduction
+# + A conclusion
+# + It should check if the user is correct and stop giving hints if they are
+# + It should give points based on how quickly the user got it correct
+print("guessing game")
+print("hint 1: they are a videogame character")
+input("guess 1:")
+print("hint 2: they have their own videogame franchise named after them")
+input("guess 2:")
+print("final hint: They are an animal but they are not much like the animal in real life")
+input("final guess:")
+print("the answer was Sonic the hedgehog!")
+print("did you get it?")
