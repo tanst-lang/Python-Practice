a877c128cd3f040d612b268d7790e779ac73d735
tanst-lang <tanst@wsc.school.nz>
2026-05-11 12:06:42 +1200

    Create quiz.py

---
+++ quiz.py
@@ -0,0 +1,10 @@
+
+question_1=input("what day is it? ")
+answer_1= "monday"
+if question_1==answer_1 :
+    print("correct!")
+question_2=input("what month is it? ")
+answer_2= "may"
+if question_2==answer_2 :
+    print("correct!")
+    
